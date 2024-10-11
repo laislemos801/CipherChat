b88ec78bd8d0e3009c3267eb13b29825de54a5be
Lais Lemos <laisaplemos10@gmail.com>
2024-10-10 21:16:52 -0300

    Adicionei menu secundário e função de enviar mensagens

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import re
 import datetime
 from pymongo import MongoClient
 
-# Função para conectar ao banco de dados e retornar a coleção de usuários
-def connect_db():
-    client = MongoClient(
-        'mongodb+srv://ana:ana39@consultas.2opj3.mongodb.net/?retryWrites=true&w=majority&appName=Consultas'
-    )
-    db = client["aulapython"]
-    return db.users  # Retorna a coleção de usuários
+# Conectar ao banco de dados uma única vez
+client = MongoClient(
+    'mongodb+srv://ana:ana39@consultas.2opj3.mongodb.net/?retryWrites=true&w=majority&appName=Consultas'
+)
+db = client["aulapython"]
+db_users = db.users  # Coleção de usuários
+db_messages = db.messages  # Coleção de mensagens
 
 # Função que valida a senha
 def validar_senha(password):
@@ -33,20 +33,21 @@ def validar_senha(password):
 
 # Função de cadastro que verifica usuário e senha
 def cadastro():
-    db_users = connect_db()
-
-    #Solicita o email e senha
+    # Solicita o email
     email = input("Digite seu e-mail: ")
-    password = input("Digite sua senha: ")
 
     # Verifica se o e-mail já está cadastrado
     if db_users.find_one({"email": email}):
         print("E-mail já cadastrado. Tente novamente.")
         return False
 
-    # Valida a senha
-    if not validar_senha(password):
-        return False
+    # Loop para solicitar a senha até que ela seja válida
+    while True:
+        password = input("Digite sua senha: ")
+        if validar_senha(password):
+            break  # Sai do loop se a senha for válida
+        else:
+            print("Tente novamente.")
 
     # Cria um documento para o novo usuário
     novo_usuario = {
@@ -59,24 +60,98 @@ def cadastro():
     db_users.insert_one(novo_usuario)
     return True
 
-def login ():
-   db_users = connect_db()
+# Função de login
+def login():
+    # Solicita o email e senha
+    email = input("Digite seu e-mail: ")
+    password = input("Digite sua senha: ")
 
-   # Solicita o email e senha
-   email = input("Digite seu e-mail: ")
-   password = input("Digite sua senha: ")
+    if db_users.find_one({"email": email, "password": password}):
+        return email  # Retorna o e-mail do usuário logado
+    else:
+        return None
 
-   if db_users.find_one({"email": email, "password": password}):
-       return True
-   else:
-        return False
+# Função para enviar uma mensagem
+def enviar_mensagem(usuario):
+    # Recupera todos os usuários cadastrados no banco de dados
+    usuarios_cadastrados = list(db_users.find({}, {"email": 1, "_id": 0}))  # Retorna apenas os e-mails
+
+    if not usuarios_cadastrados:
+        print("Nenhum usuário cadastrado encontrado.")
+        return
+
+    # Exibe os usuários cadastrados com numeração
+    print("Usuários cadastrados:")
+    for idx, user in enumerate(usuarios_cadastrados, start=1):
+        print(f"{idx}) {user['email']}")
+
+    # Solicita que o remetente escolha o destinatário pela numeração
+    while True:
+        try:
+            escolha = int(input("Escolha o número do destinatário: "))
+            if 1 <= escolha <= len(usuarios_cadastrados):
+                destinatario = usuarios_cadastrados[escolha - 1]['email']
+                break
+            else:
+                print("Opção inválida, escolha um número válido.")
+        except ValueError:
+            print("Entrada inválida, digite um número.")
+
+    # Solicita a mensagem
+    mensagem = input("Digite sua mensagem: ")
+
+    # Cria um documento de mensagem
+    nova_mensagem = {
+        "remetente": usuario,
+        "destinatario": destinatario,
+        "mensagem": mensagem,
+        "data": datetime.datetime.now(tz=datetime.timezone.utc)
+    }
+
+    # Insere a mensagem no banco de dados
+    db_messages.insert_one(nova_mensagem)
+    print(f"Mensagem enviada com sucesso para {destinatario}!")
+
+
+# Função para ler mensagens
+def ler_mensagens(usuario):
+    mensagens = db_messages.find({"destinatario": usuario})
+
+    if mensagens.count() == 0:
+        print("Nenhuma mensagem encontrada.")
+    else:
+        for msg in mensagens:
+            print(f"De: {msg['remetente']}, Mensagem: {msg['mensagem']}")
+
+# Função que exibe o menu após login bem-sucedido
+def menu_usuario(usuario):
+    while True:
+        print("\nEscolha uma opcao:")
+        print("(1) Enviar mensagem")
+        print("(2) Ler mensagens")
+        print("(3) Sair")
+        option = input("Escolha uma opção: ")
+
+        if option == "1":
+            enviar_mensagem(usuario)
+        elif option == "2":
+            ler_mensagens(usuario)
+        elif option == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
 
 # Função principal
 def main():
-
     print("Bem-vindo ao CipherChat!")
     while True:
-        option = input("Você deseja (1) cadastrar | (2) fazer login? | (3) Sair do programa: ")
+        print("\nVocê deseja:")
+        print("(1) Fazer cadastro")
+        print("(2) Fazer login")
+        print("(3) Sair do programa")
+        option = input("Escolha uma opção: ")
+
         if option == "1":
             print("Cadastre-se")
             if cadastro():
@@ -85,18 +160,17 @@ def main():
                 print("Erro no cadastro.")
         elif option == "2":
             print("Login")
-            if login():
+            usuario = login()
+            if usuario:
                 print("Login realizado com sucesso!")
+                menu_usuario(usuario)  # Mostra o menu após login
             else:
                 print("Erro no login.")
-
         elif option == "3":
             print("Saindo do programa...")
             break
         else:
             print("Opção inválida. Tente novamente.")
-
-
 
 if __name__ == '__main__':
     main()
